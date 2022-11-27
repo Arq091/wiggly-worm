759da932ec7e620481d45faa0e604f9150922632
Korn <79989518+kornls@users.noreply.github.com>
2022-11-27 16:00:17 +0700

    Implemented 1.1.0 (#5)
    * fix 2
    
    * Revert "Revamp"
    
    * fix test 1
    
    * fix test 2
    
    * fix test 3
    
    * fix test 3 (at this point literally anything cld go wrong)

--- mini/app.js
+++ mini/app.js
@@ -4,12 +4,12 @@ function vertical() {
         document.documentElement.style
             .setProperty('--animation', 'translateX(20%)');
         document.getElementById("worm").style.flexDirection = "column";
-        document.getElementById("n10").style.flexDirection = "column";
+        document.getElementById("n3").style.flexDirection = "column";
     } else {
         document.documentElement.style
             .setProperty('--animation', 'translateY(20%)');
         document.getElementById("worm").style.flexDirection = "row";
-        document.getElementById("n10").style.flexDirection = "row";
+        document.getElementById("n3").style.flexDirection = "row";
     }
 }
 
