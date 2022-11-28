c9abee0a3a4b7f1c8077fe50ff9b55e1178c27d7
Korn <79989518+kornls@users.noreply.github.com>
2022-11-28 10:39:15 +0700

    beta2

--- normal/app.js
+++ normal/app.js
@@ -33,8 +33,8 @@ function setsize() {
 function randworm() {
 	rand.color == Math.floor(Math.random() * 360);
 	rand.size == Math.floor(Math.random() * 100) + 1;
-	document.documentElement.style.setProperty('--box-color', rand.color + 'px');
-	document.documentElement.style.setProperty.('--worm-size', rand.size + 'px');
+	document.documentElement.style.setProperty('--box-color', rand.color);
+	document.documentElement.style.setProperty('--worm-size', rand.size + 'px');
 }	
 
 function home() {
