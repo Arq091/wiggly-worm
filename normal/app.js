4db981550f768607e3d35e9c4ee1432a8c2d1baa
Korn <79989518+kornls@users.noreply.github.com>
2022-11-28 10:22:51 +0700

    Random feature?

--- normal/app.js
+++ normal/app.js
@@ -21,14 +21,21 @@ function setcolor() {
 function setsize() {
     if (sz.value == '') {
         document.documentElement.style
-		.setProperty('--worm-size', '50px');
+			.setProperty('--worm-size', '50px');
     } else if (isNaN(parseInt(sz.value))) {
-	alert("\"" + sz.value + "\" is not a number. Please recheck your input. Note that NO UNITS are needed."); 
+		alert("\"" + sz.value + "\" is not a number. Please recheck your input. Note that NO UNITS are needed."); 
     } else {
-	document.documentElement.style
-            .setProperty('--worm-size', sz.value + 'px');
+		document.documentElement.style
+			.setProperty('--worm-size', sz.value + 'px');
     }
 }
+
+function randworm() {
+	rand.color == Math.floor(Math.random() * 360);
+	rand.size == Math.floor(Math.random() * 100) + 1;
+	document.documentElement.style.setProperty('--box-color', rand.color + 'px');
+	document.documentElement.style.setProperty.('--worm-size', rand.size + 'px');
+}	
 
 function home() {
     window.location.assign("../")
